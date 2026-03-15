7ad40b02870099f30cf0347e64acee7368be2ee6
DingkunYao <dyao0004@student.monash.edu>
2026-03-15 14:34:25 +1100

    1. sunscr and weather sql

---
+++ uv_level_monitor/core/initial_sunscr_sugg.sql
@@ -0,0 +1,15 @@
+INSERT INTO sunscr_sugg (sunscr_degree, description) VALUES
+(4, 'Low protection'),
+(6, 'Low protection'),
+(8, 'Low protection'),
+(10, 'Low protection'),
+
+(15, 'Medium protection'),
+(20, 'Medium protection'),
+(25, 'Medium protection'),
+
+(30, 'High protection'),
+(40, 'High protection'),
+(50, 'High protection'),
+
+(51, 'Very high protection');  -- 51 represent 50+ SPF level
---
+++ uv_level_monitor/core/initial_weather_table.sql
@@ -0,0 +1,39 @@
+INSERT INTO weather (weather_id, weather_name, weather_icon_id) VALUES
+(0, 'Clear', 0),
+(1, 'Clear', 1),
+(2, 'Cloudy', 2),
+(3, 'Cloudy', 3),
+
+(45, 'Fog', 45),
+(48, 'Fog', 48),
+
+(51, 'Drizzle', 51),
+(53, 'Drizzle', 53),
+(55, 'Drizzle', 55),
+
+(56, 'FrzDrizzle', 56),
+(57, 'FrzDrizzle', 57),
+
+(61, 'Rain', 61),
+(63, 'Rain', 63),
+(65, 'Rain', 65),
+
+(66, 'FrzRain', 66),
+(67, 'FrzRain', 67),
+
+(71, 'Snow', 71),
+(73, 'Snow', 73),
+(75, 'Snow', 75),
+
+(77, 'SnowGrain', 77),
+
+(80, 'Shower', 80),
+(81, 'Shower', 81),
+(82, 'Shower', 82),
+
+(85, 'SnowShow', 85),
+(86, 'SnowShow', 86),
+
+(95, 'Storm',95),
+(96, 'StormHail', 96),
+(99, 'StormHail', 99);
